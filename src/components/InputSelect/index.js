b8b2c610fea3da41695bf829a1fc2d85bbbf033e
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-16 19:05:25 -0300

    add

---
+++ src/components/InputSelect/index.js
@@ -0,0 +1,17 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+
+import { useField } from '@rocketseat/unform';
+
+import { Container } from './styles';
+
+export default function InputSelect({
+  name,
+  label,
+  options,
+  multiple,
+  ...rest
+}) {
+  return <div />;
+}
